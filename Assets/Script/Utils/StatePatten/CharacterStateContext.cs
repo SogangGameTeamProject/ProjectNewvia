45d35e86ea736f795a55399323f2fdbfa04cf974
roy5820 <troy5830@gmail.com>
2024-06-12 16:59:03 +0900

    디자인 패턴 적용

---
+++ Assets/Script/Utils/StatePatten/CharacterStateContext.cs
@@ -0,0 +1,31 @@
+namespace Newvia
+{
+    public class CharacterStateContext
+    {
+        public CharacterState CurrentState
+        {
+            get; set;
+        }
+
+        private readonly CharacterInit characterInit;
+
+
+        public CharacterStateContext(CharacterInit characterInit)
+        {
+            this.characterInit = characterInit;
+        }
+
+        //현재 상태 업데이트
+        public void Transition()
+        {
+            CurrentState.Handle(characterInit);
+        }
+
+        //상태 전환
+        public void Transition(CharacterState state, params object[] datas)
+        {
+            CurrentState = state;
+            CurrentState.Handle(characterInit, datas);
+        }
+    }
+}
---
+++ Assets/Script/Utils/StatePatten/StateBase.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Newvia {
+    public abstract class StateBase : MonoBehaviour, CharacterState
+    {
+        protected CharacterInit _CharacterInit = null;//캐릭터 컨트롤러
+
+        //기능 구현 부분
+        public void Handle(CharacterInit init, params object[] datas)
+        {
+            //캐릭터 값 초기화
+            if (!this._CharacterInit)
+                this._CharacterInit = init;
+        }
+    }
+}

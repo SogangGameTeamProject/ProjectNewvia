f71f4a043e4c0ff36ee8471e4dc5e2b92f858e74
roy5820 <35182721+roy5820@users.noreply.github.com>
2024-07-08 23:56:25 +0900

    적 오브젝트 플레이어 추적 구현

--- Assets/Script/Character/Enemy/EnemyController.cs
+++ Assets/Script/Character/Enemy/EnemyController.cs
@@ -6,7 +6,7 @@ namespace Newvia
 {
     public class EnemyController : CharacterInit
     {
-        
+
     }
 }
 
---
+++ Assets/Script/Character/Enemy/NavTest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavTest : MonoBehaviour
+{
+    public Transform target; // 플레이어의 Transform
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
